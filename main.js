baa2dfd906a843523c6c01af61464325c2cd8e3f
MaNnu81 <mannuspamm@gmail.com>
2025-03-06 11:41:10 +0100

    copiato ed integrato

--- main.js
+++ main.js
@@ -1,32 +1,74 @@
 
 
-//rgbString, hex, array, object
+// //rgbString, hex, array, object
 
-function randomColorArray() {
+// function randomColorArray() {
    
+//     const r = Math.round(Math.random() * 255);
+//     const g = Math.round(Math.random() * 255);
+//     const b = Math.round(Math.random() * 255);
+ 
+//     return [r, g, b]
+// }
+
+// function randomColorHex() {
+//     // let letters = '0123456789ABCDEF';
+//     // let color = '#'
+
+//     // for (let i = 0; i < 6; i++) {
+//     //    color += letters[Math.floor(Math.random() * 16)]
+        
+//     // }
+//     // return color
+// }
+
+// function randomColorRgb() {
+//     let [r, g, b] = randomColorArray();
+//     return `rgb(${r}, ${g}, ${b})`;
+// }
+// const randomColor = randomColorRgb()
+// console.log(randomColor)
+
+
+// function randomColorObj() {
+//     let [r, g, b] = randomColorRgb();
+//     return {}
+// }
+
+
+
+function randomColorArray(){
     const r = Math.round(Math.random() * 255);
     const g = Math.round(Math.random() * 255);
-    const b= Math.round(Math.random() * 255);
- 
-    return [r, g, b]
+    const b = Math.round(Math.random() * 255);
+
+    return [r, g, b];
 }
 
-function randomColorHex() {
-    // let letters = '0123456789ABCDEF';
-    // let color = '#'
 
-    // for (let i = 0; i < 6; i++) {
-    //    color += letters[Math.floor(Math.random() * 16)]
-        
-    // }
-    // return color
+function randomColorHex(){
+    const cArray = randomColorArray();
+    // const r = (('0' + cArray[0].toString(16)).slice(-2));
+    // const g = (('0' + cArray[1].toString(16)).slice(-2));
+    // const b = (('0' + cArray[2].toString(16)).slice(-2));
+
+    const r = cArray[0].toString(16).padStart(2, '0');
+    const g = cArray[1].toString(16).padStart(2, '0');
+    const b = cArray[2].toString(16).padStart(2, '0');
+
+
+    return '#' + r + g + b;
 }
 
-function randomColorObj() {
-    
+
+function randomColorRgb(){
+    const cArray = randomColorArray();
+    return `rgb(${cArray[0]},${cArray[1]},${cArray[2]})`
 }
 
-console.log(randomColorArray());
-console.log(randomColorHex());
-// console.log(randomColorRgb());
-// console.log(randomColorObj());
+
+function randomColorObj(){
+    const cArray = randomColorArray();
+    return {r: cArray[0], g:cArray[1], b:cArray[2]}
+}
+
